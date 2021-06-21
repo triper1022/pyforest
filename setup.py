5229a938a3e145b357ce523d8634f1bc072d3fb3
Marco, Chang <35587460+triper1022@users.noreply.github.com>
2021-06-21 11:05:45 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ from pkg_resources import VersionConflict, require
 from setuptools import setup
 from src.pyforest.auto_import import setup as setup_auto_import
 
+from src.pyforest.user_specific_imports import _load_user_specific_imports
+#(globals_: dict, user_imports_path=USER_IMPORTS_PATH)
+
 try:
     require("setuptools>=38.3")
 except VersionConflict:
@@ -20,3 +23,4 @@ if __name__ == "__main__":
     setup()
     setup_auto_import()
     # extensions cannot be installed because pyforest is only available after the installation
+    _load_user_specific_imports(globes(), "my common use import.txt")
